7b408c329ebc07f3d4479d19c0d71c943a44f1f4
Anderson <anderson.lemos@dmorj.com.br>
2014-07-18 11:34:43 -0300

    Atualizado

--- CarrinhoDeCompras/MaiorPrecoTest.cs
+++ CarrinhoDeCompras/MaiorPrecoTest.cs
@@ -5,25 +5,30 @@ namespace CarrinhoDeCompras
     [TestFixture]
     public class MaiorPrecoTest
     {
+        private CarrinhoDeCompras _carrinho;
+
+        [SetUp]
+        void InicializarCarrinho()
+        {
+            this._carrinho=new CarrinhoDeCompras();
+        }
+
+
         [Test]
         public void DeveRetornarZeroSeCarrinhoVazio()
         {
 
-            var carrinho = new CarrinhoDeCompras();
-
-            Assert.AreEqual(0.0,carrinho.MaiorValor(),0.0000001);
+            Assert.AreEqual(0.0,_carrinho.MaiorValor(),0.0000001);
 
         }
 
         [Test]
-        public void DeveRetornarValorDoItemSeCarrinhoContem1Elemento()
+        public void DeveRetornarValorDoItemSeCarrinhoContem1Elemento() 
         {
 
-            var carrinho = new CarrinhoDeCompras();
+            _carrinho.Adiciona(new Item("Geladeira",1,900.0));
 
-            carrinho.Adiciona(new Item("Geladeira",1,900.0));
-
-            Assert.AreEqual(900.0, carrinho.MaiorValor(), 0.0000001);
+            Assert.AreEqual(900.0, _carrinho.MaiorValor(), 0.0000001);
 
         }
 
@@ -31,13 +36,12 @@ namespace CarrinhoDeCompras
         public void DeveRetornarMaiorValorSeCarrinhoContemMuitosElementos()
         {
 
-            var carrinho = new CarrinhoDeCompras();
-
-            carrinho.Adiciona(new Item("Geladeira", 1, 900.0));
-            carrinho.Adiciona(new Item("Fogão", 1, 1500.0));
-            carrinho.Adiciona(new Item("Máquina de Lavar", 1, 750.0));
+      
+            _carrinho.Adiciona(new Item("Geladeira", 1, 900.0));
+            _carrinho.Adiciona(new Item("Fogão", 1, 1500.0));
+            _carrinho.Adiciona(new Item("Máquina de Lavar", 1, 750.0));
             
-            Assert.AreEqual(1500.0, carrinho.MaiorValor(), 0.0000001);
+            Assert.AreEqual(1500.0, _carrinho.MaiorValor(), 0.0000001);
         }
 
     }
