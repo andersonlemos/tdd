c5955b8206df20246be5a3bc7587fb6130ec3d14
Anderson <anderson.lemos@dmorj.com.br>
2014-09-04 17:42:58 -0300

    Problema da Nota Fiscal Incluido

--- CalculoDeSalario/Cargo.cs
+++ CalculoDeSalario/Cargo.cs
@@ -5,6 +5,7 @@
        public static Cargo DESENVOLVEDOR{get{return new Cargo(new DezOuVintePorCento());}}
        public static Cargo DBA { get { return new Cargo(new QuinzeOuVinteECincoPorCento());}}
        public static Cargo TESTADOR { get { return new Cargo(new QuinzeOuVinteECincoPorCento());}}
+       public static Cargo ESTAGIARIO { get { return new Cargo(new QuinzeOuVinteECincoPorCento()); } }
       
        public IRegraDeCalculo Regra { get; private set; }
 
