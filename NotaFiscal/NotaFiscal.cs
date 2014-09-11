5f2239a99286ef153063a45c3e4e361b489f32f9
Anderson <anderson.lemos@dmorj.com.br>
2014-09-11 17:46:05 -0300

    Inclusao das classes Pedido e NotaFiscal

---
+++ NotaFiscal/NotaFiscal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotaFiscal
+{
+   public class NotaFiscal
+    {
+       public string Cliente{get;private set;}
+       public double Valor { get; private set; }
+       public DateTime Data { get; private set; }
+
+
+       public NotaFiscal(string cliente,double valor,DateTime data)
+       {
+           this.Cliente = cliente;
+           this.Valor = valor;
+           this.Data = data;
+       }
+
+
+    }
+}
---
+++ NotaFiscal/Pedido.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotaFiscal
+{
+  public  class Pedido
+    {
+
+      public string Cliente { get; private set; }
+      public double ValorTotal { get; private set; }
+      public int QuantidadeItens{get;private set;}
+      
+      public Pedido(string cliente,double valorTotal,int quantidadeItens)
+      {
+          this.Cliente = cliente;
+          this.ValorTotal = valorTotal;
+          this.QuantidadeItens = quantidadeItens;
+          
+      }
+      
+
+    }
+}
