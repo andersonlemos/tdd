b445f078fdbf8b2f7d6b7f8863f050b036be6493
Anderson <anderson.lemos@dmorj.com.br>
2014-09-12 17:52:22 -0300

    Incluida classe Geradora de NotaFiscal

---
+++ NotaFiscal/GeradorDeNotaFiscal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotaFiscal
+{
+   public class GeradorDeNotaFiscal
+    {
+       public NotaFiscal Gera(Pedido pedido)
+       {
+           
+           return new NotaFiscal(pedido.Cliente,pedido.ValorTotal * 0.94,DateTime.Now);
+
+       }
+
+
+    }
+}
--- NotaFiscal/NUnitTestFixture.cs
+++ NotaFiscal/NUnitTestFixture.cs
@@ -9,8 +9,16 @@ namespace NotaFiscal
     public class NUnitTestFixture
     {
         [Test]
-        public void Test()
+        public void DeveGerarNfComValorDeImpostoDescontado()
         {
+            GeradorDeNotaFiscal gerador=new GeradorDeNotaFiscal();
+
+            Pedido pedido=new Pedido("Mauricio",1000,1);
+
+            NotaFiscal nf = gerador.Gera(pedido);
+
+
+            Assert.AreEqual(1000 * 0.94,nf.Valor,0.0001);
 
         }
     }
