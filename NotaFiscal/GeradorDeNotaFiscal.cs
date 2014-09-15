3080fa853663ddecbf8ad858cdbacd94ca2795de
Anderson <anderson.lemos@dmorj.com.br>
2014-09-15 17:59:48 -0300

    Mock nota fiscal

--- NotaFiscal/GeradorDeNotaFiscal.cs
+++ NotaFiscal/GeradorDeNotaFiscal.cs
@@ -7,12 +7,27 @@ using System.Threading.Tasks;
 namespace NotaFiscal
 {
    public class GeradorDeNotaFiscal
-    {
+   {
+
+       private NFDao dao;
+
+
+       public GeradorDeNotaFiscal(NFDao dao)
+       {
+           this.dao = dao;
+       }
+
+
+
        public NotaFiscal Gera(Pedido pedido)
        {
            
-           return new NotaFiscal(pedido.Cliente,pedido.ValorTotal * 0.94,DateTime.Now);
+           //return new NotaFiscal(pedido.Cliente,pedido.ValorTotal * 0.94,DateTime.Now);
+           NotaFiscal nf = new NotaFiscal(pedido.Cliente, pedido.ValorTotal * 0.94, DateTime.Now);
 
+           dao.Persiste(nf);
+
+           return nf;
        }
 
 
--- NotaFiscal/NUnitTestFixture.cs
+++ NotaFiscal/NUnitTestFixture.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Moq;
 using NUnit.Framework;
 
 namespace NotaFiscal
@@ -11,15 +12,29 @@ namespace NotaFiscal
         [Test]
         public void DeveGerarNfComValorDeImpostoDescontado()
         {
-            GeradorDeNotaFiscal gerador=new GeradorDeNotaFiscal();
+            //GeradorDeNotaFiscal gerador=new GeradorDeNotaFiscal();
 
-            Pedido pedido=new Pedido("Mauricio",1000,1);
+            //Pedido pedido=new Pedido("Mauricio",1000,1);
+
+            //NotaFiscal nf = gerador.Gera(pedido);
+
+
+            //Assert.AreEqual(1000 * 0.94,nf.Valor,0.0001);
+
+        }
+
+        [Test]
+        public void DevePersistirNotaFiscalGerada()
+        {
+            var dao = new Mock<NFDao>();
+            GeradorDeNotaFiscal gerador=new GeradorDeNotaFiscal(dao.Object);
+
+            Pedido pedido = new Pedido("Mauricio", 1000, 1);
 
             NotaFiscal nf = gerador.Gera(pedido);
 
 
-            Assert.AreEqual(1000 * 0.94,nf.Valor,0.0001);
-
+            dao.Verify(t=>t.Persiste(nf));
         }
     }
 }
